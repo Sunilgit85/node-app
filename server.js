0f0e98310cb7aaa3ce5958b61cd58d29aaa57a47
Sunilgit85 <53135335+Sunilgit85@users.noreply.github.com>
2019-08-11 21:16:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:blue;">Sunil_World  App - CI/CD Jenkins first dev!!</h1> \n');
+  res.send('<h1 style="color:golden;">Sunil_World  App - CI/CD Jenkins first dev!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
